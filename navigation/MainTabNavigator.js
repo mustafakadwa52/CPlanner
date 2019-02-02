50f911cf7b6517e64f100178fb5db6da400db215
Mustafa <mustafa.kadwa52@gmail.com>
2019-02-02 23:16:52 +0300

    Update MainTabNavigator.js
    tab navigator adding createMenuItem Screen

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -6,6 +6,7 @@ import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+import CreateMenuItemScreen from "../screens/Menu/CreateMenuItem";
 
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
@@ -53,8 +54,23 @@ SettingsStack.navigationOptions = {
   ),
 };
 
+const MenuStack = createStackNavigator({
+  createMenu: CreateMenuItemScreen,
+});
+
+MenuStack.navigationOptions = {
+  tabbBarLabel: 'Settings',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+    />
+  ),
+};
+
 export default createBottomTabNavigator({
   HomeStack,
   LinksStack,
   SettingsStack,
+  MenuStack,
 });
